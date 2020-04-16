b06c34caa8c6d830c333d56aa5976ff4492e2c53
Cairo Pereira <cairo.151295@gmail.com>
2020-04-15 22:36:41 -0300

    Empty project

---
+++ src/main/java/br/edu/udf/bdadvanced/BdadvancedApplication.java
@@ -0,0 +1,13 @@
+package br.edu.udf.bdadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdadvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdadvancedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/udf/bdadvanced/BdadvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.udf.bdadvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdadvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
