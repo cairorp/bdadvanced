8403ff85a86d57a7b7b3cc4826dc0651123f466b
Cairo Pereira <cairo.151295@gmail.com>
2020-04-17 17:47:25 -0300

    Criar exceção para tratar erros de autenticação.

--- src/main/java/br/edu/udf/bdadvanced/exception/AuthenticationException.java
+++ src/main/java/br/edu/udf/bdadvanced/exception/AuthenticationException.java
@@ -1,5 +1,9 @@
 package br.edu.udf.bdadvanced.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "Login ou senha incorretos!")
 public class AuthenticationException extends IllegalArgumentException {
 
     public AuthenticationException(String message) {
