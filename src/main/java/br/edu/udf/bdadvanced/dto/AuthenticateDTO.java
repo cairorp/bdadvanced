e027b0029bd7f6ee6d948559bbaba1da0937c65b
Cairo Pereira <cairo.151295@gmail.com>
2020-04-17 17:44:33 -0300

    Criar DTO para retornar dados necessarios para uma autenticação.

--- src/main/java/br/edu/udf/bdadvanced/dto/AuthenticateDTO.java
+++ src/main/java/br/edu/udf/bdadvanced/dto/AuthenticateDTO.java
@@ -3,7 +3,7 @@ package br.edu.udf.bdadvanced.dto;
 import lombok.Getter;
 
 @Getter
-public class LoginDTO {
+public class AuthenticateDTO {
     private String login;
     private String password;
 }
