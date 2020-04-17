5ebbe383ce7ae41231076b12b8eebf44db68c02b
Cairo Pereira <cairo.151295@gmail.com>
2020-04-17 17:45:18 -0300

    Criar DTO para receber login e senha e validar se os dois estão corretos.

---
+++ src/main/java/br/edu/udf/bdadvanced/dto/UserDTO.java
@@ -0,0 +1,11 @@
+package br.edu.udf.bdadvanced.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+    private Integer cdUser;
+    private String firstName;
+}
